a109b157b368e8ed6ab9a6b24d9294b96a49a0aa
Raafat-Samy-Labib <raafatsamy696@gmail.com>
2021-05-06 20:14:19 +0200

    create components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule ,Routes } from '@angular/router';
+import { BookComponent } from './components/book/book.component';
+import { BrowseComponent } from './components/browse/browse.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+
+
+const routes: Routes = [
+  {path:'',component:HeaderComponent} , 
+  {path:'home',component:HomeComponent},
+  {path:'books',component:BookComponent},
+  {path:'browse',component:BrowseComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+   
+];
+
+
+@NgModule({
+  imports: [
+      RouterModule.forRoot(routes)
+    ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BookComponent } from './components/book/book.component';
+import { BrowseComponent } from './components/browse/browse.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, BookComponent, BrowseComponent, LoginComponent, RegisterComponent, HeaderComponent, FooterComponent],
+  imports: [
+    BrowserModule,
+     HttpClientModule,
+     AppRoutingModule,
+    
+    ],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
