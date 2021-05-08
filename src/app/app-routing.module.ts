6b1c1aa1b275ed06045a4a8feadbc3b15eeb953f
Raafat-Samy-Labib <raafatsamy696@gmail.com>
2021-05-08 16:07:30 +0200

    install libs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule ,Routes } from '@angular/router';
 import { BookComponent } from './components/book/book.component';
 import { BrowseComponent } from './components/browse/browse.component';
-import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -11,7 +10,7 @@ import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
-  {path:'',component:HeaderComponent} , 
+   
   {path:'home',component:HomeComponent},
   {path:'books',component:BookComponent},
   {path:'browse',component:BrowseComponent},
