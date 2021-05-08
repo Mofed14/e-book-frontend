faaa2bb4084983f95acc00f923425987bfaec135
mofed salah <mofed.salah.hana@gmail.com>
2021-05-08 17:25:44 +0200

    change one of routes and delete one package

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule ,Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { BookComponent } from './components/book/book.component';
 import { BrowseComponent } from './components/browse/browse.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
-
-
-
 const routes: Routes = [
-   
-  {path:'home',component:HomeComponent},
-  {path:'books',component:BookComponent},
-  {path:'browse',component:BrowseComponent},
-  {path:'login',component:LoginComponent},
-  {path:'register',component:RegisterComponent},
-   
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'books', component: BookComponent },
+  { path: 'browse', component: BrowseComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
-
 @NgModule({
-  imports: [
-      RouterModule.forRoot(routes)
-    ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
