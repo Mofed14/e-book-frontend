9022cd21b20cf8fbcc55d684c23845b2113b27bf
mofed salah <mofed.salah.hana@gmail.com>
2021-05-11 04:35:39 +0200

    homepage get all data from database and pagination

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css','./search.css']
+  styleUrls: ['./homepage.component.css', './search.css'],
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  books: any;
+  limit = 8;
+  items: any;
+  constructor(private api: ApiService, private toastr: ToastrService) {}
 
   ngOnInit(): void {
+    this.listAllBooks(this.limit);
   }
 
+  // tslint:disable-next-line:typedef
+  listAllBooks(limit) {
+    this.api.getAllBooks(limit).subscribe((res) => {
+      if (res.books.length) {
+        this.books = res.books;
+        console.log(this.books.length);
+        // console.log(this.books.title);
+      } else {
+        this.toastr.warning('No books yet');
+      }
+    });
+  }
+
+  getMore() {
+    this.books.push(this.listAllBooks((this.limit += 8)));
+    // console.log(this.books.push(this.listAllBooks(this.limit + 8)));
+  }
+
+  getless() {
+    this.books.push(this.listAllBooks((this.limit -= 8)));
+    // console.log(this.books.push(this.listAllBooks(this.limit + 8)));
+  }
 }
