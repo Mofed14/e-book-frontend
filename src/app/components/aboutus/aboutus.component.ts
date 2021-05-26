2a32e4525ffd3c7899dd008b07150388c27fa6f3
mofed salah <mofed.salah.hana@gmail.com>
2021-05-26 21:10:09 +0200

    wowjs

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgwWowService } from 'ngx-wow';
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.css'],
 })
 export class AboutusComponent implements OnInit {
-  constructor() {}
+  constructor(private wow: NgwWowService) {
+    this.wow.init();
+  }
 
   ngOnInit(): void {}
 }
